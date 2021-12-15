9a7f7654c23bd148ae2877c5993733dcf27e8f01
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2021-12-15 18:27:55 +0330

    feat: add timeline-event component

--- src/views/portfolio.view.vue
+++ src/views/portfolio.view.vue
@@ -1,11 +1,38 @@
 <template>
-  <div>portfolio</div>
+  <div class="portfolio">
+    <timeline-event>
+      <div class="portfolio__event">
+        <p>Born In 1998</p>
+      </div>
+    </timeline-event>
+    <timeline-event>
+      <div class="portfolio__event">
+        <p>Born In 1998</p>
+      </div>
+    </timeline-event>
+    <timeline-event>
+      <div class="portfolio__event">
+        <p>Born In 1998</p>
+      </div>
+    </timeline-event>
+  </div>
 </template>
 
 <script>
+  import timelineEvent from '@/common/components/timeline-event.component.vue';
   export default {
     name: 'portfolio-view',
+    components: { timelineEvent },
   };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .portfolio {
+    $self: &;
+
+    margin: 64px;
+
+    #{ $self }__event {
+    }
+  }
+</style>
