2faad20dd48ef850b7f559074c509526ddc71fa0
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2021-12-15 21:32:10 +0330

    feat: add timline album

--- src/common/view-components/timeline-album.component.vue
+++ src/common/view-components/timeline-album.component.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
-    <h2>The Beginning</h2>
-    <p>I was born in NewYork city at the year 1998. </p>
-  </div>
+  <h2 class="title">The Beginning</h2>
+  <p>Written the songs while i was at the camp.</p>
+  <album-cover class="album-cover"></album-cover>
 </template>
 
 <script>
+  import albumCover from '@/common/components/album-cover.component.vue';
   export default {
     name: 'timeline-album',
+    components: { albumCover },
   };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .title {
+    margin-bottom: 6px;
+  }
+
+  .album-cover {
+    margin: 24px 0 8px;
+  }
+</style>
