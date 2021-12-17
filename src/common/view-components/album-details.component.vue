ac0f86318f753f8a1987076d6869acdde5a4a613
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2021-12-17 12:21:07 +0330

    feat: add name and date

--- src/common/view-components/album-details.component.vue
+++ src/common/view-components/album-details.component.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>album details</div>
+  <h3 class="album-name">The Underground World</h3>
+  <p class="caption">
+    Release Date:
+    <span class="overline-1 date">2013/05/06</span>
+  </p>
 </template>
 
 <script>
@@ -8,4 +12,13 @@
   };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .album-name {
+    margin: 24px 0 8px;
+  }
+
+  .date {
+    color: var(--text-color-87);
+    margin: 0 4px;
+  }
+</style>
