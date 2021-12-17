9bb690059979e3c8b6ad672fadb1220bbf7f9dd0
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2021-12-17 11:19:58 +0330

    feat: add album details

---
+++ src/common/view-components/album-details.component.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>album details</div>
+</template>
+
+<script>
+  export default {
+    name: 'album-details',
+  };
+</script>
+
+<style lang="scss" scoped></style>
--- src/common/view-components/timeline-album.component.vue
+++ src/common/view-components/timeline-album.component.vue
@@ -2,13 +2,15 @@
   <h2 class="title">The Beginning</h2>
   <p>Written the songs while i was at the camp.</p>
   <album-cover class="album-cover"></album-cover>
+  <album-details></album-details>
 </template>
 
 <script>
   import albumCover from '@/common/components/album-cover.component.vue';
+  import albumDetails from '@/common/view-components/album-details.component.vue';
   export default {
     name: 'timeline-album',
-    components: { albumCover },
+    components: { albumCover, albumDetails },
   };
 </script>
 
@@ -19,5 +21,6 @@
 
   .album-cover {
     margin: 24px 0 8px;
+    cursor: pointer;
   }
 </style>
