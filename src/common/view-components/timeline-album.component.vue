f215013269f6cfefe3d8f54ec0f649835913b362
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2021-12-17 12:25:17 +0330

    feat: add more details button

--- src/common/view-components/timeline-album.component.vue
+++ src/common/view-components/timeline-album.component.vue
@@ -3,14 +3,19 @@
   <p>Written the songs while i was at the camp.</p>
   <album-cover class="album-cover"></album-cover>
   <album-details></album-details>
+  <ripple-button class="more" trailing-icon-name="arrow_circle_right">
+    More Details
+  </ripple-button>
 </template>
 
 <script>
   import albumCover from '@/common/components/album-cover.component.vue';
+  import rippleButton from '@/common/components/ripple-button.component.vue';
   import albumDetails from '@/common/view-components/album-details.component.vue';
+
   export default {
     name: 'timeline-album',
-    components: { albumCover, albumDetails },
+    components: { rippleButton, albumCover, albumDetails },
   };
 </script>
 
@@ -22,5 +27,9 @@
   .album-cover {
     margin: 24px 0 8px;
     cursor: pointer;
+  }
+
+  .more {
+    margin-top: 16px;
   }
 </style>
