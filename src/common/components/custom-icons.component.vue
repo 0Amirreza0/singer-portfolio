581cb861d2f208f43ad06c069b016fd6785b4677
Amirreza Saadati <amirreza.saadati.c@gmail.com>
2021-12-17 11:58:58 +0330

    feat: add custom icon component

---
+++ src/common/components/custom-icons.component.vue
@@ -0,0 +1,31 @@
+<script>
+  import { h } from 'vue';
+
+  import album from '@/assets/icons/album.svg';
+  import arrowCircleRight from '@/assets/icons/arrow-circle-right.svg';
+
+  const iconMap = {
+    album,
+    arrow_circle_right: arrowCircleRight,
+  };
+
+  export default {
+    name: 'custom-icons',
+    props: {
+      name: {
+        type: String,
+        required: true,
+      },
+    },
+
+    computed: {
+      iconComponent() {
+        return iconMap[this.name];
+      },
+    },
+
+    render() {
+      return h(this.iconComponent);
+    },
+  };
+</script>
